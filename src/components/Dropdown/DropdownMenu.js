1c6d5063b6da885c4003bbf0b5d0600202936c93
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-02-13 21:38:34 +0200

    +

---
+++ src/components/Dropdown/DropdownMenu.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+
+import { Menu, Icon, message } from 'antd'
+
+
+const openNotification = () => {
+  message.info("This is a normal message");
+};
+
+const style = {
+  marginLeft: "6px"
+}
+
+const DropdownMenu = () => (
+  <Menu onClick={() => openNotification()}>
+    <Menu.Item key="site-overview">
+      <Icon type="eye-o" />
+      <span style={style}>
+        Overview
+      </span>
+    </Menu.Item>
+    <Menu.Item key="marketing">
+      <Icon type="area-chart" />
+      <span style={style}>
+        Marketing
+      </span>
+    </Menu.Item>
+    <Menu.Item key="activity">
+      <Icon type="calendar" />
+      <span style={style}>
+        Activity
+      </span>
+    </Menu.Item>
+  </Menu>
+)
--- src/components/Dropdown/HeaderDropdown.js
+++ src/components/Dropdown/HeaderDropdown.js
@@ -1,38 +1,8 @@
 import React, { Component } from 'react'
 
-import { Dropdown, Menu, Icon, message } from 'antd';
+import { Dropdown, Menu, Icon, message } from 'antd'
 
 
-const openNotification = () => {
-  message.info("This is a normal message");
-};
-
-const style = {
-  marginLeft: "6px"
-}
-
-const DropdownMenu = () => (
-  <Menu onClick={() => openNotification()}>
-    <Menu.Item key="site-overview">
-      <Icon type="eye-o" />
-      <span style={style}>
-        Overview
-      </span>
-    </Menu.Item>
-    <Menu.Item key="marketing">
-      <Icon type="area-chart" />
-      <span style={style}>
-        Marketing
-      </span>
-    </Menu.Item>
-    <Menu.Item key="activity">
-      <Icon type="calendar" />
-      <span style={style}>
-        Activity
-      </span>
-    </Menu.Item>
-  </Menu>
-)
 
 const HeaderDropdown = () => {
 
